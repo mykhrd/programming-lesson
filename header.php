641a1bee6e41d222ce770286107f839c98b3b6eb
Miyuki Niimi <mykhrd@gmail.com>
2022-12-15 13:43:06 +0900

    fix

--- header.php
+++ header.php
@@ -5,17 +5,16 @@
  <title>サイトタイトル</title>
  <meta name="description" content="ディスクリプションを入力">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link href="<?php echo get_template_directory_uri(); ?>/reset.css" rel="stylesheet">
  <link href="<?php echo get_template_directory_uri(); ?>/dist/css/style.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&family=Shippori+Mincho&display=swap" rel="stylesheet">
 <link rel="stylesheet "href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/slick/slick.css">
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/slick/slick-theme.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/plugin/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/plugin/slick/slick-theme.css">
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/plugin/slick/slick.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/dist/js/scripts.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/dist/js/slick.min.js"></script>
 </head>  
 <body>
 <?php wp_head(); ?>
